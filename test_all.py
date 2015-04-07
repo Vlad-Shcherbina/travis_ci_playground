4456c018063c2715ae52e03627772d623adb0264
Vlad-Shcherbina <vlad.shcherbina@gmail.com>
2015-04-07 21:06:04 +0200

    Update test_all.py

--- test_all.py
+++ test_all.py
@@ -17,6 +17,10 @@ def test_square_is_even(x):
     assume(x % 2 == 0)
     logging.info('x = {}'.format(x))
     assert x ** 2 % 2 == 0, x
+
+
+def test_skipped():
+    raise nose.SkipTest('zzz')
     
 
 if __name__ == '__main__':
