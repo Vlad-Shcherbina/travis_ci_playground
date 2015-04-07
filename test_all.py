0fc04e2059dbe43970efa130a892b1aa96e88a30
Vlad-Shcherbina <vlad.shcherbina@gmail.com>
2015-04-07 20:57:20 +0200

    Update test_all.py

--- test_all.py
+++ test_all.py
@@ -1,9 +1,9 @@
 from __future__ import print_function
 import sys
+import logging
 
 import nose
 from hypothesis import given, assume
-
 
 N = 41
 
@@ -14,8 +14,14 @@ def example_test():
 
 @given(int)
 def test_square_is_even(x):
-    assume(x % 2 == 0)
+    assume(x % 2 == 0 or x == 7)
+    logging.info('x = {}'.format(x))
     assert x ** 2 % 2 == 0, x
+    
+    
+def test_failing():
+    logging.info('hello')
+    assert False
 
 
 if __name__ == '__main__':
@@ -23,5 +29,6 @@ if __name__ == '__main__':
     argv = sys.argv + [
         '--verbose', '--with-doctest',
         '--logging-level=DEBUG',
+        '--rednose',
         ]
     nose.run_exit(argv=argv)
