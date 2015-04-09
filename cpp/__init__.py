b34c1f11047b292e1878d31d5656eb6ecf4ab806
Vlad Shcherbina <vlad.shcherbina@gmail.com>
2015-04-09 21:19:37 +0200

    add missing __init__.py

---
+++ cpp/__init__.py
@@ -0,0 +1,21 @@
+import os
+
+def build_extensions():
+    from distutils.core import setup, Extension
+    cur_dir = os.getcwd()
+    try:
+        os.chdir(os.path.dirname(os.path.abspath(__file__)))
+        setup(
+            name='sample',
+            py_modules=['sample'],
+            ext_modules=[
+                Extension('_sample',
+                    ['sample.i', 'sample.cpp'],
+                    depends=['sample.h'],
+                    swig_opts=['-c++'],
+                ),
+            ],
+            script_args=['build_ext', '--inplace']
+        )
+    finally:
+        os.chdir(cur_dir)
