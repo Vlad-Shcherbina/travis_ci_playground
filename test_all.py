4d6573cee3f5d3592cd60650cf2496da29115f14
Vlad Shcherbina <vlad.shcherbina@gmail.com>
2015-04-09 21:14:47 +0200

    add swig stuff

--- test_all.py
+++ test_all.py
@@ -3,8 +3,12 @@ import sys
 import logging
 
 import nose
-from hypothesis import given, assume
 import numpy
+
+import cpp
+cpp.build_extensions()
+from cpp import sample
+
 
 N = 41
 
@@ -13,22 +17,29 @@ def example_test():
     pass
 
 
-@given(int)
-def test_square_is_even(x):
-    assume(x % 2 == 0)
-    logging.info('x = {}'.format(x))
-    assert x ** 2 % 2 == 0, x
-
-
 def test_skipped():
     raise nose.SkipTest('zzz')
-    
-    
+
+
 def test_numpy():
     xs = numpy.array([0, 10, 0, 20, 0])
     nz, = xs.nonzero()
     assert numpy.array_equal(nz, numpy.array([1, 3]))
-    
+
+
+def test_swig():
+    assert sample.N == 42
+    assert sample.square_float(2) == 4.0
+    assert sample.reverse([1, 2, 3]) == (3, 2, 1)
+
+    hz = sample.Hz()
+    hz.a = 1
+    hz.b = 'b'
+    hz2 = sample.Hz(hz)
+    hz.a = 7
+    assert str(hz) == 'Hz(7, b)'
+    assert str(hz2) == 'Hz(1, b)'
+
 
 if __name__ == '__main__':
     print(sys.version)
